ce07453d85789a22336ff81792c83fbd0a5fd0bd
uborzz <uborzz@gmail.com>
2020-06-29 01:42:17 +0200

    fixed router.use instead of get

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -7,7 +7,7 @@ const productsRouter = require("./routes/products");
 app.set("views", path.join(__dirname + "/views"));
 app.set("view engine", "pug");
 
-app.get("/products", productsRouter);
+app.use("/products", productsRouter);
 
 const server = app.listen(8000, function () {
   console.log(`Listening http://localhost:${server.address().port}`);
