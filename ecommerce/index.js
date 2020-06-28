2405f051feaac1c07e7c271738a30e3284f8be49
uborzz <uborzz@gmail.com>
2020-06-29 01:43:26 +0200

    statics and views with bulma

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -3,6 +3,8 @@ const path = require("path")
 const app = express();
 const productsRouter = require("./routes/products");
 
+app.use("/static", express.static(path.join(__dirname, "public")));
+
 // app.engine("jsx", expressJsx);  # por defecto usa pug
 app.set("views", path.join(__dirname + "/views"));
 app.set("view engine", "pug");
